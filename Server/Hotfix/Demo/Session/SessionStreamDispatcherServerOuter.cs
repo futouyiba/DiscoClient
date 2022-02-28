13d7d88200843bf483e67fd70ae589afa1259f69
FuTou <futouyiba@gmail.com>
2022-02-28 15:28:59 +0800

    server fixing... not done, server should process IRequest, and should process actor messages.

--- Server/Hotfix/Demo/Session/SessionStreamDispatcherServerOuter.cs
+++ Server/Hotfix/Demo/Session/SessionStreamDispatcherServerOuter.cs
@@ -8,19 +8,39 @@ namespace ET
     {
         public void Dispatch(Session session, MemoryStream memoryStream)
         {
-            ushort opcode = BitConverter.ToUInt16(memoryStream.GetBuffer(), Packet.KcpOpcodeIndex);
+            // erlangMogai
+            // ushort opcode = BitConverter.ToUInt16(memoryStream.GetBuffer(), Packet.KcpOpcodeIndex);
+            var tMsg = ProtobufHelper.FromStream(typeof (TMsg), memoryStream) as TMsg;
+            if (tMsg == null)
+            {
+                Log.Error("收到一条不是我们定义tmsg的消息，跳过！");
+                return;
+            }
+            var opcode = (ushort)tMsg.type;
+            
             Type type = OpcodeTypeComponent.Instance.GetType(opcode);
-            object message = MessageSerializeHelper.DeserializeFrom(opcode, type, memoryStream);
-
+            object message = null;
+            if (tMsg.body!=null)
+            {
+                message = ProtobufHelper.FromBytes(type, tMsg.body, 0, tMsg.body.Length);
+                OpcodeHelper.LogMsg(session.DomainZone(), opcode, message);
+            }
+            else
+            {
+                message = Activator.CreateInstance(type);
+            }            
             if (message is IResponse response)
             {
+                response.Error = tMsg.error_code;
+                response.Message = tMsg.error_string;
+                response.RpcId = tMsg.rpc_id;
                 session.OnRead(opcode, response);
                 return;
             }
 
-            OpcodeHelper.LogMsg(session.DomainZone(), opcode, message);
-			
-            DispatchAsync(session, opcode, message).Coroutine();
+            // OpcodeHelper.LogMsg(session.DomainZone(), opcode, message);
+            // 普通消息或者是Rpc请求消息
+            MessageDispatcherComponent.Instance.Handle(session, opcode, message);
         }
 		
         public async ETTask DispatchAsync(Session session, ushort opcode, object message)
--- Unity/Codes/Hotfix/Demo/Login/LoginHelper.cs
+++ Unity/Codes/Hotfix/Demo/Login/LoginHelper.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 
 namespace ET
 {
-    
     public static class LoginHelper
     {
         public static string DEVICE_PRODUCT_ID = "DEVICE_PRODUCT_ID";
@@ -17,8 +16,13 @@ namespace ET
         public static int UserId75 = 32;
         public static string DeviceModel75 = "DEVICE_MODEL75";
         public static string DeviceProductId75 = "DEVICE_PRODUCT_ID75";
+
         public static async ETTask Login(Scene zoneScene, string address, string account, string password)
         {
+            var myProductId = SystemInfo.deviceUniqueIdentifier;
+            Log.Info("product id:"+myProductId);
+            
+            int.TryParse(account, out var accountInt);
             try
             {
                 // 创建一个ETModel层的Session
@@ -27,15 +31,19 @@ namespace ET
                 try
                 {
                     session = zoneScene.GetComponent<NetKcpComponent>().Create(NetworkHelper.ToIPEndPoint(address));
+                    // session = zoneScene.GetComponent<NetKcpComponent>().Create(NetworkHelper.ToIPEndPoint(ConstValue.LoginAddress));
+                    // var r2CLogin = (R2C_Login) await session.Call(new C2R_Login() { Account = "11", Password = "11" });
+                    // Log.Info("r2c login:"+r2CLogin);
+
                     // var registerResp =
-                    //         (register_user_s2c)await session.Call(new register_user_c2s()
-                    //         {
-                    //             device_product_id = DeviceProductId75, device_model = DeviceModel75,device_type = 1
-                    //         });
-                    // Debug.Log("registerResp:"+registerResp);
+                            // (register_user_s2c)await session.Call(new register_user_c2s()
+                            // {
+                                // device_product_id = DeviceProductId75, device_model = DeviceModel75, device_type = 1
+                            // });
+                    // Debug.Log("registerResp:" + registerResp);
                     getEndpointResp =
                             (get_transfer_endpoint_s2c)await session.Call(new get_transfer_endpoint_c2s() { endpoint_id = 1, user_id = UserId75 });
-                    Log.Info("endpoint response:"+getEndpointResp);
+                    Log.Info("endpoint response:" + getEndpointResp);
                 }
                 finally
                 {
@@ -43,25 +51,26 @@ namespace ET
                 }
 
                 // 创建一个gate Session,并且保存到SessionComponent中
-                Session gateSession = zoneScene.GetComponent<NetKcpComponent>().Create(NetworkHelper.ToIPEndPoint(getEndpointResp.ip, getEndpointResp.port));
+                Session gateSession = zoneScene.GetComponent<NetKcpComponent>()
+                        .Create(NetworkHelper.ToIPEndPoint(getEndpointResp.ip, getEndpointResp.port));
                 gateSession.AddComponent<PingComponent>();
                 zoneScene.AddComponent<SessionComponent>().Session = gateSession;
-				
+
                 // G2C_LoginGate g2CLoginGate = (G2C_LoginGate)await gateSession.Call(
                 //     new C2G_LoginGate() { Key = r2CLogin.Key, GateId = r2CLogin.GateId});
                 var authResp = (authenticate_s2c)await gateSession.Call(new authenticate_c2s()
                 {
                     device_product_id = DeviceProductId75, device_type = 1, user_id = UserId75
                 });
-                
-                Log.Debug("auth response:"+authResp);
 
-                await Game.EventSystem.PublishAsync(new EventType.LoginFinish() {ZoneScene = zoneScene});
+                Log.Debug("auth response:" + authResp);
+
+                await Game.EventSystem.PublishAsync(new EventType.LoginFinish() { ZoneScene = zoneScene });
             }
             catch (Exception e)
             {
                 Log.Error(e);
             }
-        } 
+        }
     }
 }
