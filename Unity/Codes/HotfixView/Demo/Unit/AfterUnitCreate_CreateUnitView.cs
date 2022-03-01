fb57d95b8bcba263715a8c23aba0deb01928783a
futouyiba <futouyiba@gmail.com>
2022-03-01 17:18:24 +0800

    game object instantiate success. position is not scaled yet.

--- Unity/Codes/HotfixView/Demo/Unit/AfterUnitCreate_CreateUnitView.cs
+++ Unity/Codes/HotfixView/Demo/Unit/AfterUnitCreate_CreateUnitView.cs
@@ -9,7 +9,7 @@ namespace ET
             // Unit View层
             // 这里可以改成异步加载，demo就不搞了
             var figureConfigId = args.Unit.ConfigId;
-            var prefabName = "cSingle" + figureConfigId;
+            var prefabName = "cSingle" +figureConfigId.ToString("00");
             GameObject bundleGameObject = (GameObject)ResourcesComponent.Instance.GetAsset("Unit.unity3d", "Unit");
             GameObject prefab = bundleGameObject.Get<GameObject>(prefabName);
 	        
