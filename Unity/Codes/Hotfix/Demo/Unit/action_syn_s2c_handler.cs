8321d98bd1d2c1ce08615864816d9f882f178729
FuTou <futouyiba@gmail.com>
2022-03-10 18:22:07 +0800

    Create action_syn_s2c_handler.cs

---
+++ Unity/Codes/Hotfix/Demo/Unit/action_syn_s2c_handler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace ET
+{
+    /// <summary>
+    /// 现在处理的是刚进入舞池时候的all sync。后续不排除有其他时机导致的all sync，届时再作修改。
+    /// </summary>
+    [MessageHandler]
+    public class action_syn_s2c_handler: AMHandler<action_syn_s2c>
+    {
+
+        protected override async ETTask Run(Session session, action_syn_s2c message)
+        {
+            Log.Info($"action sync s2c message:{message}");
+            await ETTask.CompletedTask;
+        }
+    }
+}
