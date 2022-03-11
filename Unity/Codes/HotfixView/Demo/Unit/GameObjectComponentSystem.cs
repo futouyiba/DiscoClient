bec865577b2fdd73704a4d3528cabfd696936964
FuTou <futouyiba@gmail.com>
2022-03-12 00:03:35 +0800

    Update GameObjectComponentSystem.cs

--- Unity/Codes/HotfixView/Demo/Unit/GameObjectComponentSystem.cs
+++ Unity/Codes/HotfixView/Demo/Unit/GameObjectComponentSystem.cs
@@ -23,5 +23,13 @@ namespace ET
                     // new Vector3(self.OriScale.x * scale * self.FowardDirection, self.OriScale.y * scale, self.OriScale.z * scale);
                     self.GameObject.transform.localScale = Vector3.one * 2;
         }
+
+        public static void ChangeMovingDirection(this GameObjectComponent self, float direction)
+        {
+            self.FowardDirection = direction;
+            var oldScale = self.SpriteGO.transform.localScale;
+            self.SpriteGO.transform.localScale =
+                    new Vector3(oldScale.x * direction, oldScale.y, oldScale.z);
+        }
     }
 }
