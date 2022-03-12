73fab9573e0d5954c5bb53b8d630b5c84563a20a
FuTou <futouyiba@gmail.com>
2022-03-12 08:28:35 +0800

    Update UnitFactory.cs

--- Unity/Codes/Hotfix/Demo/Unit/UnitFactory.cs
+++ Unity/Codes/Hotfix/Demo/Unit/UnitFactory.cs
@@ -8,16 +8,7 @@ namespace ET
         {
 	        UnitComponent unitComponent = currentScene.GetComponent<UnitComponent>();
 	        Unit unit = unitComponent.AddChildWithId<Unit, int>(unitInfo.UnitId, unitInfo.ConfigId);
-	        unitComponent.Add(unit);
-	        
-	        unit.Position = new Vector3(unitInfo.X, unitInfo.Y, unitInfo.Z);
-	        unit.Forward = new Vector3(unitInfo.ForwardX, unitInfo.ForwardY, unitInfo.ForwardZ);
-	        
-	        // NumericComponent numericComponent = unit.AddComponent<NumericComponent>();
-	        // for (int i = 0; i < unitInfo.Ks.Count; ++i)
-	        // {
-		       //  numericComponent.Set(unitInfo.Ks[i], unitInfo.Vs[i]);
-	        // }
+	        // unitComponent.Add(unit);
 	        
 	        unit.AddComponent<MoveComponent>();
 	        if (unitInfo.MoveInfo != null)
