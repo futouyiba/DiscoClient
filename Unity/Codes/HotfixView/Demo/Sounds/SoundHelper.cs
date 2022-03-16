d2cc832a828266f3681fd6c1a6efa9d3bd54a759
Weiland <weilandss@gmail.com>
2022-03-16 19:22:59 +0800

    无关的函数移到soundhelper里

--- Unity/Codes/HotfixView/Demo/Sounds/SoundHelper.cs
+++ Unity/Codes/HotfixView/Demo/Sounds/SoundHelper.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using ET.Demo.Sounds;
 using UnityEngine;
+using ET.Demo.Music;
 
 namespace ET.Demo.Sounds
 {
@@ -32,5 +34,48 @@ namespace ET.Demo.Sounds
                 return USoundMgr.gameObject.GetComponentInChildren<AudioSource>();
             }
         }
+        
+        /// <summary>
+        /// 给unity中的物件挂上Monobehaviours
+        /// 基本上是与节奏有关的东西
+        /// </summary>
+        public static void AttachAllUnityComponents()
+        {
+            //Speakers
+            GameObject[] speakers = GameObject.FindGameObjectsWithTag("Speaker");
+            AttachUnityComponent<uBeatResponserScale>(speakers);
+            //Cameras
+            Camera[] cameras = Camera.allCameras;
+            List<GameObject> cameraGOs = new List<GameObject>();
+            foreach (Camera camera in cameras)
+            {
+                cameraGOs.Add(camera.gameObject);
+            }
+            //初始化一下
+            List<uBeatResponsorCamera> cameraBeaters = new List<uBeatResponsorCamera>();
+            cameraBeaters = AttachUnityComponent<uBeatResponsorCamera>(cameraGOs) as List<uBeatResponsorCamera>;
+            for (int i = 0; i < cameras.Length; i++)
+            {//todo:read from config
+                cameraBeaters[i].Init(cameras[i], .5f);
+            }
+
+        }
+
+        /// <summary>
+        /// 给场景里的speaker类东西随低频波动的类
+        /// </summary>
+        /// <param name="gameObjects"></param>
+        public static IEnumerable<T> AttachUnityComponent<T>(IEnumerable<GameObject> gameObjects) where T:UnityEngine.Component
+        {
+            List<T> attached=new List<T>();
+            foreach (GameObject gameObject in gameObjects)
+            {
+                var addcomponent = gameObject.AddComponent<T>();
+
+                attached.Add(addcomponent);
+            }
+            return attached;
+        }
+
     }
 }
