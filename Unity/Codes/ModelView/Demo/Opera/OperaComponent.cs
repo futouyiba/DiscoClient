4e862dad651c840e307efc886f71c77c61477933
FuTou <futouyiba@gmail.com>
2022-03-28 14:59:33 +0800

    Try syncing editor build settings

--- Unity/Codes/ModelView/Demo/Opera/OperaComponent.cs
+++ Unity/Codes/ModelView/Demo/Opera/OperaComponent.cs
@@ -12,6 +12,8 @@ namespace ET
 	    public int mapMask;
 	    public GameObject DjGO;
 	    public GameObject DiscoCamera;
+	    public GameObject TeleportStartParticleGO;
+	    public GameObject TeleportEndParticleGO;
 
 	    public readonly C2M_PathfindingResult frameClickMap = new C2M_PathfindingResult();
 	    public readonly action_req_c2s move_action_req_c2s = new action_req_c2s(){action_id = 2};
